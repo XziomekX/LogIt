58e2e1ce01aa025ac8f7bab4d483ecfc3d7de63d
LucasEasedUp <lucaseasedup@gmail.com>
2013-07-10 14:29:44 +0200

    Update year in license notices.

--- src/io/github/lucaseasedup/logit/TickEventCaller.java
+++ src/io/github/lucaseasedup/logit/TickEventCaller.java
@@ -1,7 +1,7 @@
 /*
  * TickEventCaller.java
  *
- * Copyright (C) 2012 LucasEasedUp
+ * Copyright (C) 2012-2013 LucasEasedUp
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
