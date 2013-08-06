b7a6e291c1a523df23b96d7cd3799691876ce1ee
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-06 19:55:23 +0200

    Add property validators

---
+++ src/io/github/lucaseasedup/logit/config/validators/DbTypeValidator.java
@@ -0,0 +1,40 @@
+/*
+ * DbTypeValidator.java
+ *
+ * Copyright (C) 2012-2013 LucasEasedUp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.lucaseasedup.logit.config.validators;
+
+import io.github.lucaseasedup.logit.config.PropertyType;
+import io.github.lucaseasedup.logit.config.PropertyValidator;
+
+/**
+ * @author LucasEasedUp
+ */
+public class DbTypeValidator implements PropertyValidator
+{
+    @Override
+    public boolean validate(String path, PropertyType type, Object value)
+    {
+        if (value == null)
+            return false;
+        
+        String string = value.toString();
+        
+        return string.equalsIgnoreCase("sqlite") || string.equalsIgnoreCase("mysql")
+                || string.equalsIgnoreCase("h2") || string.equalsIgnoreCase("csv-1");
+    }
+}
