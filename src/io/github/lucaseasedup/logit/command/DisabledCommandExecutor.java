bdaff000533a5019b31028e6e95b21646905cc03
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-06 16:05:14 +0200

    Add DisabledCommandExecutor and its messages

---
+++ src/io/github/lucaseasedup/logit/command/DisabledCommandExecutor.java
@@ -0,0 +1,43 @@
+/*
+ * DisabledCommandExecutor.java
+ *
+ * Copyright (C) 2012-2013 LucasEasedUp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.lucaseasedup.logit.command;
+
+import static io.github.lucaseasedup.logit.LogItPlugin.getMessage;
+import io.github.lucaseasedup.logit.LogItCore;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author LucasEasedUp
+ */
+public class DisabledCommandExecutor extends AbstractCommandExecutor
+{
+    public DisabledCommandExecutor(LogItCore core)
+    {
+        super(core);
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        sender.sendMessage(getMessage("COMMAND_DISABLED"));
+        
+        return true;
+    }
+}
