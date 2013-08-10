4cf0656cb6367986405660a9dc9e7a7ae6665b04
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-10 21:49:18 +0200

    Add missing license headers and @author tags

--- src/io/github/lucaseasedup/logit/IniFile.java
+++ src/io/github/lucaseasedup/logit/IniFile.java
@@ -1,3 +1,21 @@
+/*
+ * IniFile.java
+ *
+ * Copyright (C) 2012-2013 LucasEasedUp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.lucaseasedup.logit;
 
 import com.google.common.collect.ImmutableSet;
@@ -19,6 +37,9 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author LucasEasedUp
+ */
 public class IniFile
 {
     public IniFile()
