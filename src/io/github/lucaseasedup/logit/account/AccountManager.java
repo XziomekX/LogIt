2a7a13c1e91b937da58990ee3a5ef8d595bf142c
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-11 16:50:20 +0200

    Rename getAccounts() to getAllAccounts()

--- src/io/github/lucaseasedup/logit/account/AccountManager.java
+++ src/io/github/lucaseasedup/logit/account/AccountManager.java
@@ -447,7 +447,7 @@ public final class AccountManager extends LogItCoreObject
         }
     }
     
-    public Collection<Account> getAccounts()
+    public Collection<Account> getAllAccounts()
     {
         return accountMap.values();
     }
