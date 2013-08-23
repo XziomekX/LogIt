6f25706e95ae3137c6b078aafeeb848ccfa4151b
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-23 19:10:31 +0200

    Add simple LogItTestSuite

---
+++ src/test/java/EmailUtilsTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import io.github.lucaseasedup.logit.util.EmailUtils;
+import org.junit.Test;
+
+public class EmailUtilsTest
+{
+    @Test
+    public void test()
+    {
+        assertTrue(EmailUtils.validateEmail("example123@website321.com"));
+        assertFalse(EmailUtils.validateEmail(""));
+        assertFalse(EmailUtils.validateEmail("example"));
+        assertFalse(EmailUtils.validateEmail("example@"));
+        assertFalse(EmailUtils.validateEmail("website.com"));
+        assertFalse(EmailUtils.validateEmail("@website.com"));
+        assertFalse(EmailUtils.validateEmail("example@@website.com"));
+        assertFalse(EmailUtils.validateEmail("examp,le@website.com"));
+        assertFalse(EmailUtils.validateEmail("example@website.com "));
+        
+        try
+        {
+            assertFalse(EmailUtils.validateEmail(null));
+        }
+        catch (Throwable ex)
+        {
+        }
+    }
+}
