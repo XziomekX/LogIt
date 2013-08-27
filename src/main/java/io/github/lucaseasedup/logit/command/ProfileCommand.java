ea17f0ddea61e80c0ac9228c7bfcef3ff64be99b
LucasEasedUp <lucaseasedup@gmail.com>
2013-08-27 12:21:46 +0200

    Add code to the "/profile view" command

--- src/main/java/io/github/lucaseasedup/logit/command/ProfileCommand.java
+++ src/main/java/io/github/lucaseasedup/logit/command/ProfileCommand.java
@@ -20,6 +20,8 @@ package io.github.lucaseasedup.logit.command;
 
 import static io.github.lucaseasedup.logit.LogItPlugin.getMessage;
 import io.github.lucaseasedup.logit.LogItCoreObject;
+import io.github.lucaseasedup.logit.profile.field.Field;
+import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -39,13 +41,44 @@ public final class ProfileCommand extends LogItCoreObject implements CommandExec
         
         if (args.length == 1 && args[0].equalsIgnoreCase("view"))
         {
-            if (p != null && !p.hasPermission("logit.profile.view.self"))
+            if (p == null)
+            {
+                sender.sendMessage(getMessage("ONLY_PLAYERS"));
+            }
+            else if (!p.hasPermission("logit.profile.view.self"))
             {
                 sender.sendMessage(getMessage("NO_PERMS"));
             }
             else
             {
+                List<Field> fields = getCore().getProfileManager().getDefinedFields();
                 
+                sender.sendMessage("");
+                sender.sendMessage(getMessage("PROFILE_HEADER")
+                        .replace("%player%", p.getName()));
+                sender.sendMessage(getMessage("ORANGE_HORIZONTAL_LINE"));
+                
+                if (!fields.isEmpty())
+                {
+                    for (Field field : fields)
+                    {
+                        Object value = getCore().getProfileManager()
+                                .getProfileObject(p.getName(), field.getName());
+                        
+                        if (value == null)
+                        {
+                            value = "";
+                        }
+                        
+                        sender.sendMessage(getMessage("PROFILE_FIELD")
+                                .replace("%field%", field.getName())
+                                .replace("%value%", value.toString()));
+                    }
+                }
+                else
+                {
+                    sender.sendMessage(getMessage("PROFILE_NO_FIELDS"));
+                }
             }
         }
         else if (args.length == 2 && args[0].equalsIgnoreCase("view"))
