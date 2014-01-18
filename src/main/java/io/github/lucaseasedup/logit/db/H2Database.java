cd2c42c5a389da1861b3d16869d04bf68e67bf62
LucasEasedUp <lucaseasedup@gmail.com>
2014-01-18 11:09:25 +0100

    Change copyright year

--- src/main/java/io/github/lucaseasedup/logit/db/H2Database.java
+++ src/main/java/io/github/lucaseasedup/logit/db/H2Database.java
@@ -1,7 +1,7 @@
 /*
  * H2Database.java
  *
- * Copyright (C) 2012-2013 LucasEasedUp
+ * Copyright (C) 2012-2014 LucasEasedUp
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
