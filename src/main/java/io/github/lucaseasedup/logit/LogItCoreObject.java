cf45560e6912235dc875f05e2bb49ef60b7b1785
LucasEasedUp <lucaseasedup@gmail.com>
2014-05-07 09:59:17 +0200

    Add isCoreStarted() method

--- src/main/java/io/github/lucaseasedup/logit/LogItCoreObject.java
+++ src/main/java/io/github/lucaseasedup/logit/LogItCoreObject.java
@@ -59,6 +59,11 @@ public abstract class LogItCoreObject
         return core.getPlugin();
     }
     
+    protected final boolean isCoreStarted()
+    {
+        return core.isStarted();
+    }
+    
     protected final File getDataFolder()
     {
         return core.getPlugin().getDataFolder();
