7af31984ffbdb59fad25336e80975406d5d45970
LucasEasedUp <lucaseasedup@gmail.com>
2014-05-08 21:15:15 +0200

    Add MetricsLite

--- src/main/java/io/github/lucaseasedup/logit/LogItPlugin.java
+++ src/main/java/io/github/lucaseasedup/logit/LogItPlugin.java
@@ -43,12 +43,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
 
 public final class LogItPlugin extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
+        try
+        {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        }
+        catch (IOException ex)
+        {
+        }
+        
         try
         {
             loadMessages();
