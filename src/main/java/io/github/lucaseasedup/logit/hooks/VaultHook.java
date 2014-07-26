fcc680c04178ef62e2aa2ffca4f8463088e097e9
LucasEasedUp <lucaseasedup@gmail.com>
2014-07-26 22:10:17 +0200

    Move VaultHook to hooks

--- src/main/java/io/github/lucaseasedup/logit/hooks/VaultHook.java
+++ src/main/java/io/github/lucaseasedup/logit/hooks/VaultHook.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package io.github.lucaseasedup.logit.util;
+package io.github.lucaseasedup.logit.hooks;
 
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
