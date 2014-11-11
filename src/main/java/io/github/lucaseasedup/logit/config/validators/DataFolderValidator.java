617c670b8ec9d8a8bd081bfd78a6ff8db5fcad76
LucasEasedUp <lucaseasedup@gmail.com>
2014-11-11 17:30:25 +0100

    Add DataFileValidator and DataFolderValidator

---
+++ src/main/java/io/github/lucaseasedup/logit/config/validators/DataFolderValidator.java
@@ -0,0 +1,19 @@
+package io.github.lucaseasedup.logit.config.validators;
+
+import io.github.lucaseasedup.logit.LogItCore;
+import io.github.lucaseasedup.logit.config.PropertyType;
+import io.github.lucaseasedup.logit.config.PropertyValidator;
+
+public final class DataFolderValidator implements PropertyValidator
+{
+    @Override
+    public boolean validate(String path, PropertyType type, Object value)
+    {
+        if (value == null)
+            return false;
+        
+        return LogItCore.getInstance().getDataFile(
+                value.toString()
+        ).isDirectory();
+    }
+}
