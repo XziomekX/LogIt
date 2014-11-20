7402ee3a64a7717d5f08dfb52c7ee9a6e4ec925e
LucasEasedUp <lucaseasedup@gmail.com>
2014-11-20 20:43:31 +0100

    Cache TimeString.decode() results

--- src/main/java/io/github/lucaseasedup/logit/config/TimeString.java
+++ src/main/java/io/github/lucaseasedup/logit/config/TimeString.java
@@ -1,6 +1,8 @@
 package io.github.lucaseasedup.logit.config;
 
 import io.github.lucaseasedup.logit.config.validators.TimeStringValidator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 
 public final class TimeString
@@ -13,6 +15,14 @@ public final class TimeString
     {
         if (string == null || convertTo == null)
             throw new IllegalArgumentException();
+        
+        CacheTag cacheTag = new CacheTag(string, convertTo);
+        Long cachedResult = cache.get(cacheTag);
+        
+        if (cachedResult != null)
+        {
+            return cachedResult;
+        }
         
         Matcher matcher = TimeStringValidator.PATTERN.matcher(string);
         long time = 0;
@@ -28,6 +38,45 @@ public final class TimeString
             }
         }
         
+        cache.put(cacheTag, time);
+        
         return time;
     }
+    
+    private static final class CacheTag
+    {
+        private CacheTag(String string, TimeUnit convertTo)
+        {
+            if (string == null || convertTo == null)
+                throw new IllegalArgumentException();
+            
+            this.string = string;
+            this.convertTo = convertTo;
+        }
+        
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (!(obj instanceof CacheTag))
+                return false;
+            
+            CacheTag cacheTag = (CacheTag) obj;
+            
+            return cacheTag.string.equals(string)
+                    && cacheTag.convertTo.equals(convertTo);
+        }
+        
+        @Override
+        public int hashCode()
+        {
+            int result = string.hashCode();
+            result = 31 * result + convertTo.hashCode();
+            return result;
+        }
+        
+        private final String string;
+        private final TimeUnit convertTo;
+    }
+    
+    private static final Map<CacheTag, Long> cache = new HashMap<>();
 }
